683495f9183e14428e98b7b04627f5fefdc1f2c4
iBoBoTi <iroegbucalistus@gmail.com>
2022-10-27 15:14:12 +0100

    add the procfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/iBoBoTi/hng-task1/internal/handler"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	env := os.Getenv("GIN_MODE")
+	if env != "release" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("couldn't load env vars: %v", err)
+		}
+	}
+
+	port:= os.Getenv("PORT")
+	if port == ""{
+		port = "8080"
+	}
+
 	router := gin.Default()
 	router.GET("/my-profile", handler.HandleShowStudentProfile)
 
-	log.Println("starting server at port: 3000")
-	http.ListenAndServe(":3000", router)
+	log.Printf("starting server at port: %s", port)
+	http.ListenAndServe(fmt.Sprintf(":%s",port), router)
 
 }
