f7b525bc186edc1a48005b4e642ca1e874140bc9
iBoBoTi <iroegbucalistus@gmail.com>
2022-10-28 09:46:58 +0100

    fix: add cors settings

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,10 +5,10 @@ import (
 	"log"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/iBoBoTi/hng-task1/internal/handler"
 	"github.com/joho/godotenv"
+	"github.com/iBoBoTi/hng-task1/internal/middleware"
 )
 
 func main() {
@@ -27,7 +27,7 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Recovery())
 	// setup cors
-	router.Use(cors.Default())
+	router.Use(middleware.CORS())
 	router.GET("/my-profile", handler.HandleShowStudentProfile)
 
 	log.Printf("starting server at port: %s", port)
